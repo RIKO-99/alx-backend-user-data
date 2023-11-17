1fd311a51dbfae68d931fe1d4fb51e6008fdb864
RIKO-99 <rikohssn@gmail.com>
2023-11-17 22:33:58 +0000

    auth with bcrypt

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,13 +1,118 @@
-"""_import libraries
+#!/usr/bin/env python3
+"""_summary_
 """
+
+from flask import Flask, jsonify, request, abort, redirect
+from auth import Auth
 import bcrypt
 
-def _hash_password(password: str) -> bytes:
-        """_summary_
+AUTH = Auth()
 
-    Returns:
-        str: hashed passwd
+app = Flask(__name__)
+
+def _hash_password(password: str) -> bytes:
+      """hashed
     """
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
+
+@app.route('/', methods=['GET'])
+def index() -> str:
+    """_summary_
+    """
+    return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def users() -> str:
+    """_summary_"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        hashed_password = _hash_password(password)
+        user = AUTH.register_user(email, hashed_password)
+        return jsonify({"email": user.email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+
+def login() -> str:
+    """_summary_
+
+    Returns:
+        str: _description_
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not (AUTH.valid_login(email, password)):
+        abort(401)
+    else:
+        # create a new session
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie('session_id', session_id)
+
+    return response
+
+
+@app.route('/sessions', methods=['DELETE'])
+def logout() -> str:
+    """_summary_
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if not user:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect('/')
+
+
+@app.route('/profile', methods=['GET'])
+def profile() -> str:
+    """_summary_
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        return jsonify({"email": user.email}), 200
+    else:
+        abort(403)
+
+
+@app.route('/reset_password', methods=['POST'])
+def get_reset_password_token() -> str:
+    """_summary_
+
+    Returns:
+        str: _description_
+    """
+    email = request.form.get('email')
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": reset_token}), 200
+    except Exception:
+        abort(404)
+
+
+@app.route('/reset_password', methods=['PUT'])
+def update_password() -> str:
+    """_summary_
+
+    Returns:
+        str: _description_
+    """
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    new_password = request.form.get('new_password')
+    try:
+        AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"}), 200
+    except Exception:
+        abort(403)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
